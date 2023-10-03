859352cdd2c52b22d791bd972cb0d2119d53e2d5
Victor Aaron Alvarez Paredes <89317849+Vaap2099@users.noreply.github.com>
2023-10-03 16:46:57 -0600

    autenticaion de categorias

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -2,22 +2,27 @@ const express = require('express');
 
 const CategoryService = require('./../services/category.service');
 const validatorHandler = require('./../middlewares/validator.handler');
+const { checkRoles } = require('./../middlewares/auth.handler');
 const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
 const passport = require('passport');
 
 const router = express.Router();
 const service = new CategoryService();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const categories = await service.find();
+router.get('/',
+passport.authenticate('jwt',{session:false}),
+checkRoles('admin','seller','customer'),
+async(req,res,next)=>{
+  try{
+    const categories=await service.find();
     res.json(categories);
   } catch (error) {
-    next(error);
-  }
-});
+    next(error);}
+  });
 
 router.get('/:id',
+  passport.authenticate('jwt', {session:false}),
+  checkRoles('admin', 'seller' ,'customer'),
   validatorHandler(getCategorySchema, 'params'),
   async (req, res, next) => {
     try {
@@ -32,6 +37,7 @@ router.get('/:id',
 
 router.post('/',
   passport.authenticate('jwt', {session:false}),
+  checkRoles('admin', 'seller'),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
@@ -45,6 +51,8 @@ router.post('/',
 );
 
 router.patch('/:id',
+  passport.authenticate('jwt', {session:false}),
+  checkRoles('admin', 'seller'),
   validatorHandler(getCategorySchema, 'params'),
   validatorHandler(updateCategorySchema, 'body'),
   async (req, res, next) => {
@@ -60,6 +68,8 @@ router.patch('/:id',
 );
 
 router.delete('/:id',
+  passport.authenticate('jwt', {session:false}),
+  checkRoles('admin', 'seller'),
   validatorHandler(getCategorySchema, 'params'),
   async (req, res, next) => {
     try {
